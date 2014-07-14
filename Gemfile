39bc1bd41d77729968a25afac48c1f9a58373676
Tikhon Botchkarev <TikiTDO@gmail.com>
2014-07-14 04:32:17 -0400

    Clean and minimize

--- Gemfile
+++ Gemfile
@@ -3,8 +3,6 @@ source 'https://rubygems.org'
 gem 'rake'
 gem 'pry'
 gem 'andand'
-gem 'binding_of_caller'
-gem 'sourcify'
 
 # TODO: Fast Object to JSON encoder. Better performance than native JSON
 #gem 'yajl-ruby' 
--- lib/sereth_utils/all.rb
+++ lib/sereth_utils/all.rb
@@ -5,11 +5,7 @@ raise LoadError, 'Ruquires Ruby 2' if !RUBY_VERSION.match(/^2/)
 require 'rubygems'
 require 'pry'
 require 'andand'
-require 'binding_of_caller'
 
 # Use staging for all following code.
-require_relative './stage'
 require_relative './alias_args'
 require_relative './callbacks'
-
-Binding.run_stage(:sereth_util_loaded)
